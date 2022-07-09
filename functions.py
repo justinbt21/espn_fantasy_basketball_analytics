9b96d716aebfc834a27a7db4d391113b14955fc5
JT <JT@JTs-MacBook-Air.attlocal.net>
2022-07-09 14:58:14 -0700

    add files

---
+++ functions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  9 14:22:32 2022
+
+@author: JT
+"""
+
---
+++ untitled1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 17:02:55 2022
+
+@author: JT
+"""
+
