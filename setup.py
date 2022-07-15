6260f4022bc6cf448a6e6167a257f48c23cbe489
justinbt21 <justinbt21@gmail.com>
2022-07-14 17:39:27 -0700

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="espn_fantasy_basketball_analytics",
-    version="0.0.5",
+    version="0.0.6",
     author="Justin Tran",
     author_email="justinbt21@gmail.com",
     license="MIT",
